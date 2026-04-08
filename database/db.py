e17c9a05c2312516dbb85089803e5715e0e4e74c
niksonndev <niksonndev@gmail.com>
2026-04-08 16:08:55 -0300

    refactor: enhance get_connection function to enforce foreign key constraints and specify return type

--- database/db.py
+++ database/db.py
@@ -1,7 +1,10 @@
 import sqlite3
+
 from config import DB_PATH
 
-def get_connection():
+
+def get_connection() -> sqlite3.Connection:
     conn = sqlite3.connect(DB_PATH, timeout=10)
     conn.row_factory = sqlite3.Row
+    conn.execute("PRAGMA foreign_keys = ON")
     return conn
