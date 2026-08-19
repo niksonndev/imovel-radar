5c32f475abab66bdc616301533cc9a53f747670a
niksonndev <niksonndev@gmail.com>
2026-08-19 14:03:53 -0300

    ci: deploy scraper infrastructure with terraform

--- infra/oidc.tf
+++ infra/oidc.tf
@@ -127,7 +127,7 @@ resource "aws_iam_policy" "github_actions_deploy" {
   policy      = data.aws_iam_policy_document.github_actions_deploy.json
 }
 
-resource "aws_iam_role_policy_attachment" "github_actions_deploy" {
+resource "aws_iam_role_policy_attachment" "github_actions_admin" {
   role       = aws_iam_role.github_actions.name
-  policy_arn = aws_iam_policy.github_actions_deploy.arn
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
